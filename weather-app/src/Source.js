8f4a3f79a76d57fa9357276f8bdd5b33f9ccc4c2
Lique007 <angeliquehilario019@gmail.com>
2023-08-22 16:20:18 +0200

    added link about the project

---
+++ weather-app/src/Source.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function sourceInfo() {
+  return (
+    <div>
+      <a href="https://github.com/DevLique/react-weather-app">Open Sourced</a>{" "}
+      by Angelique
+    </div>
+  );
+}
--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import Source from "./Source";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -9,6 +10,7 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
+    <Source/>
   </StrictMode>
 );
 
